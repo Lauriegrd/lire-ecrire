9639cf1c61629b0f3bf0bed24db260a024dbe2c9
laurie giraud <laurie.giraud@ymail.com>
2017-04-13 14:18:37 +0200

    Create index.js

---
+++ save-php/js/index.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+});
+
+$( window ).on( "load", function() {
+
+// DRAGGABLE MENU
+  var divHover = null,
+    windowClick = false;
+
+    $(window).mousedown(function(){
+    windowClick = true;
+  });
+  
+  $(window).mouseup(function(){
+    windowClick = false;
+  });
+  
+  $('.menu').hover(function(){
+    if(divHover === null){
+      divHover = $(this);
+    }
+  }, function(){
+    if(windowClick === false){
+      divHover = null;
+      $(this).css('z-index', '0');
+    }
+  });
+  
+  $(window).mousemove(function(e){
+    if(windowClick === true && divHover != null){
+      divHover.css({ top: e.clientY - divHover.height() / 2 + 'px', left: e.clientX - divHover.width() / 2 + 'px', position: 'absolute', zIndex: '1' });
+    }
+  });
+
+// TOGGLE MENU
+  $("div#menu h1").on("click",function(){
+
+    if($(this).hasClass("active")){
+      $(".sousmenu").css("display", "block");
+      $(this).removeClass('active');
+    }
+    else {
+      $(".sousmenu").css("display", "none");
+      $(this).addClass('active');
+    }
+  });
+});
