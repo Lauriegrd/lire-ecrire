0e786dd46a0122fdc34d21408da9839b9befb7de
laurie giraud <laurie.giraud@ymail.com>
2017-04-17 15:58:46 +0200

    Update flux.php

--- save-php/flux.php
+++ save-php/flux.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 
-<title>Test bootstrap</title>
+<title>flux</title>
 <meta charset= "UTF-8" />
 <link href="css/bootstrap.css" rel="stylesheet">
 <link href="css/style.css" rel="stylesheet">
@@ -25,49 +25,71 @@
 							//faire une requête pour récupérer des éléments de la db
     						foreach($db->query('SELECT id,cover,titre,auteur,auteurlire FROM article ORDER BY id DESC') as $result){
     						 echo '<div class="col-md-3">';
+    						 echo '<div id="vignette">';
+    						 echo '<a href="article/';
+    						 echo $result['id'];
+    						 echo '">';
 					         echo '<img src="./uploads/';
 						     echo $result['cover'];
 						     echo '">';
+						     //
+						     echo '</a>';
+						     echo '</div>';
 
+						     echo '<div id="info">';
+						     echo '<hr>';
 							 echo '<p>';
 							 echo '<h1>'.$result['titre'].'</h1>';
-							 echo '<h2>'.$result['auteur'].'</h2>';
-							 echo '<h2>'.$result['auteurlire'].'</h2>';
+							 echo '<h2> dépositaire : <br> '.$result['auteur'].'</h2>';
+							 echo '<h2> auteur de lextrait : <br>'.$result['auteurlire'].'</h2>';
 							 //echo '<a href="article.php';
 							 //echo '?id=';
 							 //echo $result['id'];
 							 //echo '">';
-							 echo '<a href="article/';
-							 echo $result['id'];
-							 echo '">';
-							 echo '"go"';
-							 echo '</a>';
+		
 							 echo '</p>';
+							 echo '</div>';
 							 echo '</div>';
 							}
 							 
 							 ?>
 
-				</div>			
+				</div>	
+
+
 				<div class="col-md-2">
 					<div id="menu">
 					  <h1>navigation</h1>
-					  	<div class="sousmenu">
+					  	<div id="sousmenu">
 						  	<h2>flux</h2>
-								— <a href="display/flux.html">flux</a>
+								— <a href="flux.php">flux</a>
 							<h2>dépôt</h2>
-								— <a href="depot/formulaire.html">formulaire</a><br>
-								— <a href="depot/conditions.html">conditions</a>
+								— <a href="upload.html">formulaire</a><br>
+								— <a href="conditions.html">conditions</a>
 							<h2>iceberg</h2>
-								— <a href="iceberg/documentation.html">documentation</a><br>
-								— <a href="iceberg/intention.html">intention</a><br>
-								— <a href="iceberg/colophon.html">colophon</a><br>
-								— <a href="iceberg/cadrejuridique.html">cadre juridique</a>
+								— <a href="documentation.html">documentation</a><br>
+								— <a href="intention.html">intention</a><br>
+								— <a href="colophon.html">colophon</a><br>
+								— <a href="cadrejuridique.html">cadre juridique</a>
 								<br>
 						</div>
 					</div>
-				</div>
+					
+					<div id="contact">
+					  <h1>contact</h1>
+					  	<div id="souscontact">
+						  	<h2>adressez vos commentaires et remarques à hello@lireecrire.be</h2>
+						</div>
+					</div>
 
+					<div id="recherche">
+					  <h1>recherche</h1>
+					  	<div id="sousrecherche">
+						  	<h2>barre de recherche</h2>
+						</div>
+					</div>
+
+				</div>
 
 		</div> <!-- fermeture de la matrice -->
 
